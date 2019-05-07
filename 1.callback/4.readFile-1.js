f6126b22095ffc005d775a1363078c4ec7d5963d
weikehang <976240633@qq.com>
2019-05-07 13:45:54 +0800

    异步处理读写文件

--- 1.callback/4.readFile-1.js
+++ 1.callback/4.readFile-1.js
@@ -1,3 +1,22 @@
 let fs = require("fs");
+function after(times,callback){
+  let results = [];
+  return function (data) {
+    results.push(data);
+    if(--times === 0){
+      callback(results)
+    }
+  }
+}
 
-fs.readFile("./name.text",'')
+let fn = after(2,function (arr) {
+    console.log(arr)
+});
+
+fs.readFile("./name.txt","utf8",function (err,data) {
+  fn(data);
+});
+
+fs.readFile("./age.txt","utf8",function (err,data) {
+  fn(data);
+});
--- 1.callback/5.readFile.js
+++ 1.callback/5.readFile.js
@@ -14,7 +14,7 @@ function after(times,callback){ // 使用after函数 可以简化异步操作
 let newFn = after(3,function(arr){
     console.log(arr); // 当异步完成后触发此方法
 });
-fs.readFile('./name1.txt','utf8',function(err,data){ // 5s
+fs.readFile('./name.txt','utf8',function(err,data){ // 5s
     newFn(err,data);  // 错误优先 第一个参数永远是错误
 });
 fs.readFile('./age.txt','utf8',function(err,data){ // 3s
