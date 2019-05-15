2216bf5a5e7367b83d4d888bfe85085f83097c5b
weikehang <976240633@qq.com>
2019-05-15 13:40:17 +0800

    reduce实战

--- 2.promise/history/1/1.promise-.js
+++ 2.promise/history/1/1.promise-.js
@@ -69,6 +69,7 @@ Promise.race([p1, p2]).then((result) => {
 
 
 //延时对象 defer
+/*
 function red(url) {
    let  dfd = Promise.defer();
    fs.readFile(url,"utf8",(err,data)=>{
@@ -81,4 +82,48 @@ function red(url) {
 
 red("name.txt").then(y => {
   console.log(y);
+});*/
+//reduce
+
+var number = [65, 44, 12, 4];
+
+function getSum(total, number) {
+   return total + num;
+}
+
+//total 计算后的返回值
+//currentValue 当前元素
+//当前元素的索引
+//当前元素所属的数组元素
+
+/*
+let num = number.reduce(function (total,currentValue,currentIndex,arr) {
+  return total + currentValue
 });
+console.log(num);*/
+
+/*//计算数组中对象的和,必须提供初始值
+var initialValue = 0;
+
+var sum = [{x:1},{x:2},{x: 44},{x: 4}].reduce((accumulator,currentValue) => accumulator + currentValue.x,initialValue);
+
+console.log(sum);*/
+
+/*//将二维数组合拼一维
+var flat = [[0,1],[2,3],[4,5]].reduce(function (a,b) {
+  return a.concat(b);
+});
+
+console.log(flat);*/
+var names = ['Alice', 'Bob', 'Tiff', 'Bruce', 'Alice'];
+
+var countedNames = names.reduce(function (allNames, name) {
+  if (name in allNames) {
+    allNames[name]++
+  }else {
+    allNames[name] = 1
+  }
+  return allNames
+}, {});
+
+console.log(countedNames)
