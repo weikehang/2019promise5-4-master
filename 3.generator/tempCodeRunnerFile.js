b6dad51e2743764f423aa3baa0356f20d32d277f
weikehang <976240633@qq.com>
2019-05-17 09:47:40 +0800

    临时文件

--- 3.generator/tempCodeRunnerFile.js
+++ 3.generator/tempCodeRunnerFile.js
@@ -1,4 +1,4 @@
-let fs = require('mz/fs');
+/*let fs = require('mz/fs');
 async function read(){
         let filename = await fs.readFile('./name1.txt','utf8');
         let age = await fs.readFile(filename,'utf8');
@@ -9,4 +9,19 @@ read().then(data=>{
     console.log('s',data);
 },err=>{
     console.log(err);
-})
+})*/
+
+function * read() {
+  let a = yield 1;
+  console.log(a)
+  let b = yield (2);
+  console.log(b);
+  let c = yield (3);
+  console.log(c);
+}
+
+let it = read();
+it.next();
+it.next(100);
+it.next(200);
+it.next(300);
